c9d931b7c22aa768eb5b1078d3f247bddbb967dd
Ilya Amelevich <ilya.amelevich@gmail.com>
2023-04-01 22:20:06 +0200

    test: add MustRegister tests

--- plugin_test.go
+++ plugin_test.go
@@ -1,10 +1,12 @@
 package pocketbase_plugin_ngrok
 
 import (
+	"net/http"
 	"testing"
 
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/tests"
 )
 
 func TestPlugin_Validate(t *testing.T) {
@@ -109,5 +111,90 @@ func TestPlugin_Validate(t *testing.T) {
 				t.Errorf("Validate() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
+	}
+}
+
+func TestPlugin_MustRegister(t *testing.T) {
+	// setup the test ApiScenario app instance
+	setupTestApp := func(options *Options) func() (*tests.TestApp, error) {
+		return func() (*tests.TestApp, error) {
+			testApp, err := tests.NewTestApp()
+			if err != nil {
+				return nil, err
+			}
+
+			MustRegister(testApp, options)
+
+			return testApp, nil
+		}
+	}
+
+	proxyDestinationServer := &http.Server{
+		Addr: "localhost:1234",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("OK from " + r.URL.Path))
+		}),
+	}
+	defer proxyDestinationServer.Close()
+	go proxyDestinationServer.ListenAndServe()
+
+	scenarios := []tests.ApiScenario{
+		{
+			Name:            "/ request should be proxied when enabled",
+			Method:          http.MethodPost,
+			Url:             "/",
+			ExpectedStatus:  200,
+			ExpectedContent: []string{`OK from /`},
+			TestAppFactory: setupTestApp(&Options{
+				Enabled: true,
+				Url:     "http://localhost:1234",
+			}),
+		},
+		{
+			Name:            "/ shouldn be proxied when options nil",
+			Method:          http.MethodPost,
+			Url:             "/",
+			ExpectedStatus:  404,
+			ExpectedContent: []string{`"data":{}`},
+			TestAppFactory:  setupTestApp(nil),
+		},
+		{
+			Name:            "/ shouldn be proxied when disabled",
+			Method:          http.MethodPost,
+			Url:             "/",
+			ExpectedStatus:  404,
+			ExpectedContent: []string{`"data":{}`},
+			TestAppFactory: setupTestApp(&Options{
+				Enabled: false,
+				Url:     "http://localhost:1234",
+			}),
+		},
+		{
+			Name:            "/api/test request should not be proxied when enabled",
+			Method:          http.MethodPost,
+			Url:             "/api/test",
+			ExpectedStatus:  404,
+			ExpectedContent: []string{`"data":{}`},
+			TestAppFactory: setupTestApp(&Options{
+				Enabled: true,
+				Url:     "http://localhost:1234",
+			}),
+		},
+		{
+			Name:            "/_/test request should not be proxied when enabled",
+			Method:          http.MethodPost,
+			Url:             "/_/test",
+			ExpectedStatus:  405,
+			ExpectedContent: []string{`"data":{}`},
+			TestAppFactory: setupTestApp(&Options{
+				Enabled: true,
+				Url:     "http://localhost:1234",
+			}),
+		},
+	}
+
+	for _, scenario := range scenarios {
+		scenario.Test(t)
 	}
 }
