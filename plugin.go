49219982b20909d9d6e49b82d54dd3034b8f956a
Ilya Amelevich <ilya.amelevich@gmail.com>
2023-04-01 23:16:16 +0200

    feat: add `SetSkipper` method to `Plugin`, that let change skipper in proxy middleware

--- plugin.go
+++ plugin.go
@@ -12,6 +12,11 @@ import (
 	"github.com/pocketbase/pocketbase/core"
 )
 
+// DefaultSkipper skip proxy middleware for requests, where path starts with /_/ or /api/.
+func DefaultSkipper(c echo.Context) bool {
+	return strings.HasPrefix(c.Request().URL.Path, "/_/") || strings.HasPrefix(c.Request().URL.Path, "/api/")
+}
+
 // Options defines optional struct to customize the default plugin behavior.
 type Options struct {
 	// Enabled defines if proxy should be enabled.
@@ -35,6 +40,9 @@ type Plugin struct {
 
 	// parsedUrl from options.Url
 	parsedUrl *url.URL
+
+	// Skipper function for proxy middleware
+	skipper middleware.Skipper
 }
 
 // Validate plugin options. Return error if some option is invalid.
@@ -67,22 +75,34 @@ func (p *Plugin) Validate() error {
 	return nil
 }
 
+/*
+SetSkipper set skipper function that should return true if that route shouldn't be proxied.
+
+If not set, the DefaultSkipper is used:
+
+If set - you should also control the middleware behavior for /_/ and /api/ routes.
+
+Example:
+
+	plugin.SetSkipper(func(c echo.Context) bool {
+		return c.Request().URL.Path == "/my-super-secret-route"
+	})
+*/
+func (p *Plugin) SetSkipper(skipper middleware.Skipper) {
+	p.skipper = skipper
+}
+
 func (p *Plugin) enableProxy(e *core.ServeEvent) error {
 	if p.options.Enabled {
-		// Skip PocketBase routes
-		skipperFunc := func(c echo.Context) bool {
-			return strings.HasPrefix(c.Request().URL.Path, "/_/") || strings.HasPrefix(c.Request().URL.Path, "/api/")
-		}
-
 		if p.options.ProxyLogsEnabled {
 			e.Router.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
-				Skipper: skipperFunc,
+				Skipper: p.skipper,
 			}))
 		} else {
 			log.Println("Proxy logs are disabled")
 		}
 		e.Router.Use(middleware.ProxyWithConfig(middleware.ProxyConfig{
-			Skipper: skipperFunc,
+			Skipper: p.skipper,
 			Balancer: middleware.NewRoundRobinBalancer([]*middleware.ProxyTarget{
 				{
 					URL: p.parsedUrl,
@@ -114,7 +134,10 @@ func MustRegister(app core.App, options *Options) *Plugin {
 
 // Register registers plugin.
 func Register(app core.App, options *Options) (*Plugin, error) {
-	p := &Plugin{app: app}
+	p := &Plugin{
+		app:     app,
+		skipper: DefaultSkipper,
+	}
 
 	// Set default options
 	if options != nil {
